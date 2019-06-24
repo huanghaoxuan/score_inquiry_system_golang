65b524398d79cdf81dfa8bb590ebc1c679553be4
huanghaoxuan <huanghaoxuan1998@outlook.com>
2019-06-24 12:52:59 +0800

    数据库配置文件更新

--- db/msserver.go
+++ db/msserver.go
@@ -1,9 +1,13 @@
 package db
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mssql"
+	"io"
+	"os"
+	"strings"
 )
 
 /**
@@ -16,10 +20,35 @@ import (
 
 var DB = Init()
 
+func read() map[string]string {
+	var properties = make(map[string]string)
+	file, _ := os.Open("db/db.properties")
+	defer file.Close()
+	buf := bufio.NewReader(file)
+	for {
+		line, err := buf.ReadString('\n')
+		line = strings.TrimSpace(line)
+		n := strings.Split(line, "=")
+		properties[n[0]] = n[1]
+		if err != nil {
+			if err == io.EOF {
+				fmt.Println("File read ok!")
+				break
+			} else {
+				fmt.Println("Read file error!", err)
+			}
+		}
+	}
+	return properties
+}
+
 //初始化数据库驱动
 func Init() *gorm.DB {
-	userName := "sa"
-	userPassword := "123456"
+
+	//读取配置文件
+	properties := read()
+	userName := properties["userName"]
+	userPassword := properties["userPassword"]
 	fmt.Println(userName, userPassword)
 	db, err := gorm.Open("mssql", "sqlserver://"+userName+":"+userPassword+"@localhost:1433?database=score_inquiry_system")
 	if err != nil {
@@ -30,6 +59,7 @@ func Init() *gorm.DB {
 	db.LogMode(true)
 	db.DB().SetMaxIdleConns(10)
 	db.DB().SetMaxOpenConns(100)
+	db.AutoMigrate()
 	return db
 }
 
